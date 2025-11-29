9711db15b60c4b0a2132fdf2fdefc9f173f2498f
0teklee <leetekwoo@gmail.com>
2025-11-29 16:54:26 +0900

    fix: cli updated crawling depth opt

--- s2n/s2nscanner/cli/mapper.py
+++ s2n/s2nscanner/cli/mapper.py
@@ -12,6 +12,9 @@ def cliargs_to_scanrequest(args: CLIArguments) -> ScanRequest:
         raise ValidationError(f"Invalid URL format: {args.url}")
     
     # TODO: Depth param 추가
+   
+    depth = int(args.depth) if args.depth else 2
+    
     
     # AuthType 매핑
     auth_type = None
